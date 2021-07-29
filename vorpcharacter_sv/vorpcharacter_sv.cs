ebba3b1d7ed9beb4656acfa3500a82d9aae60399
Nick Strohm <nick.strohm99@gmail.com>
2021-07-29 23:54:28 +0200

    Fixes

--- vorpcharacter_sv/vorpcharacter_sv.cs
+++ vorpcharacter_sv/vorpcharacter_sv.cs
@@ -4,14 +4,6 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.Dynamic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace vorpcharacter_sv
 {
